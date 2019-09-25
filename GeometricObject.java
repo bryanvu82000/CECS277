68bc98ee20e0b6de357366c9484d88247bfda1df
bryanvu82000 <33305885+bryanvu82000@users.noreply.github.com>
2019-09-25 09:25:11 -0700

    Update GeometricObject.java
    added part 2 (implementing Comparable)

--- GeometricObject.java
+++ GeometricObject.java
@@ -1,6 +1,6 @@
 package ipi;
 
-public abstract class GeometricObject implements GeometricObjectInterface {
+public abstract class GeometricObject implements GeometricObjectInterface,Comparable <GeometricObject> {
 	Point location; 
 	//Area()
 	public abstract double Area();
@@ -10,4 +10,22 @@ public abstract class GeometricObject implements GeometricObjectInterface {
 
 	//toString()
 	public abstract String toString();
+	
+	@Override
+	public int compareTo(GeometricObject compare) {
+		double firstArea = this.Area();
+		double secondArea = compare.Area();
+		if (firstArea > secondArea) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+	public GeometricObject compareBiggerObject (GeometricObject one, GeometricObject two) {
+			if (one.compareTo(two) < 0) {
+				return one;
+			} else {
+				return two;
+			}
+	}
 }
